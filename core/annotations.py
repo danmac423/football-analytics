8ab62b18da0bfffade304e30796abf654f8f7793
Natalia <npieczko23@gmail.com>
2024-11-24 12:51:38 +0100

    FA-30 Refactor players annotation function to use supervision library

--- core/annotations.py
+++ core/annotations.py
@@ -3,91 +3,38 @@ from PIL import Image, ImageDraw
 import cv2
 import numpy as np
 from ultralytics import YOLO
+import supervision as sv
 
 
-PLAYERS_DETECTION_MODEL = YOLO("models/football-player-detector-x.pt").to("mps")
+PLAYERS_DETECTION_MODEL = YOLO("models/football-player-detector-s.pt").to("mps")
+
+CUSTOM_PALLETE = sv.ColorPalette(
+    {
+        0: sv.Color(255, 0, 0),  # ball
+        1: sv.Color(0, 0, 255),  # goalkeeper
+        2: sv.Color(0, 255, 0),  # player
+        3: sv.Color(255, 255, 0),  # referee
+    }
+)
+
+ELLIPSE_ANNOTATOR = sv.EllipseAnnotator(color=CUSTOM_PALLETE)
 
 
-def run_player_detection(source_video_path, player_label="player", score_threshold=0.1):
+def run_player_detection(source_video_path, score_threshold=0.1):
     """
     Annotates players in video frames
     """
+
     for frame in video_frames_generator(source_video_path):
         image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-        results = PLAYERS_DETECTION_MODEL(image, nms=False, conf=0.8, imgsz=1280)[0]
-        detections = make_detections(results)
+        results = PLAYERS_DETECTION_MODEL(image, nms=False, conf=score_threshold, imgsz=1280)[0]
+        detections = sv.Detections.from_ultralytics(results)
 
-        image = draw_annotation(image, detections)
+        image = ELLIPSE_ANNOTATOR.annotate(image, detections)
 
         annotated_frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
         yield annotated_frame
-
-
-def make_detections(results):
-    """
-    Extracts detections from the model results
-    """
-
-    detections = []
-    if results.boxes is not None:
-        for box in results.boxes:
-            cls = int(box.cls[0])
-            score = float(box.conf[0])
-            bbox = box.xyxy[0].tolist()
-
-            label = results.names[cls]
-            detections.append((label, bbox, score))
-
-    return detections
-
-
-def draw_annotation(image, detections):
-    """
-    Draws annotations on the image
-    """
-
-    color = {"player": "red", "ball": "blue", "goalkeeper": "green", "referee": "yellow"}
-
-    draw = ImageDraw.Draw(image)
-    for label, bbox, score in detections:
-        x_center = (bbox[0] + bbox[2]) / 2
-        y_center = (bbox[1] + bbox[3]) / 2
-        y_bottom = bbox[3]
-        y_top = bbox[1]
-
-        radius = int(((bbox[2] - bbox[0]) + (bbox[3] - bbox[1])) / 4)
-        # draw.ellipse(
-        #     [x_center - radius, y_center - radius, x_center + radius, y_center + radius],
-        #     outline=color[label],
-        #     width=5,
-        # )
-
-        # draw.pieslice(
-        #     [x_center - radius, y_bottom - radius, x_center + radius, y_bottom + radius],
-        #     start=0,
-        #     end=180,
-        #     fill=None,
-        #     outline=color[label],
-        #     width=3,
-        # )
-
-        flag_width = int((bbox[2] - bbox[0]) * 0.6)
-        flag_height = 20
-
-        draw.polygon(
-            [
-                (x_center - flag_width / 2, y_top - 10 - flag_height),  # Lewy górny róg
-                (x_center + flag_width / 2, y_top - 10 - flag_height),  # Prawy górny róg
-                (x_center, y_top - 10),  # Wierzchołek flagi
-            ],
-            fill=color[label],
-        )
-
-        text_position = (x_center - flag_width / 4, y_bottom + 15)
-        draw.text(text_position, f"{label}: {score:.2f}", fill="black")
-
-    return image
 
 
 if __name__ == "__main__":
