be385b81db7d5a793751784453e43cf5f7de242d
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2024-12-03 20:23:37 +0100

    FA-27 Add write_to_json and read_from_json functions

---
+++ football_analytics/config_io.py
@@ -0,0 +1,18 @@
+import json
+
+from pathlib import Path
+
+
+def write_to_json(path: Path, data: dict[any: any]) -> None:
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def read_from_json(path: Path) -> list[dict[any: any]]:
+    with open(path, 'r') as file:
+        data = json.load(file)
+
+        if isinstance(data, dict):
+            return [data]
+        else:
+            return data
