b26b1543773dc412976cecff02fe39d77b64e8eb
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2024-12-28 23:45:14 +0100

    FA-38 Add training with additional dataset

--- football_analytics/modeling/train.py
+++ football_analytics/modeling/train.py
@@ -1,10 +1,11 @@
-import argparse
+from datetime import datetime
 from pathlib import Path
 
 import typer
 from loguru import logger
 from ultralytics import YOLO
 
+from football_analytics.config import RUNS_DIR
 from football_analytics.config_io import read_from_json
 
 
@@ -26,7 +27,25 @@ def main(training_config_path: Path):
     logger.info(f"Reading configuration from {training_config_path}")
 
     for config in read_from_json(training_config_path):
-        train(config)
+        current_timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        logger.info(f"Current timestamp: {current_timestamp}")
+
+        if "project" not in config.keys():
+            config["project"] = RUNS_DIR / f"{config["model"][:-3]}" / f"train_{current_timestamp}"
+
+        logger.info(f"Saving training run to: {config['project']}")
+
+        if "additional_dataset" in config.keys():
+            additional_dataset = config.pop("additional_dataset")
+
+            train(config)
+
+            config["data"] = additional_dataset
+            config["model"] = config["project"] / "train" / "weights" / "best.pt"
+
+            train(config)
+        else:
+            train(config)
 
 
 if __name__ == "__main__":
