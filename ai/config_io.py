489d3a8d23d8b317f2efa426fe96cc21135f0072
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-07 17:01:03 +0100

    FA-44 Fix typy hint for list of directonaries

--- ai/config_io.py
+++ ai/config_io.py
@@ -1,14 +1,15 @@
 import json
 
 from pathlib import Path
+from typing import Any
 
 
-def write_to_json(path: Path, data: dict[any:any]) -> None:
+def write_to_json(path: Path, data: list[dict[Any, Any]]) -> None:
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
 
-def read_from_json(path: Path) -> list[dict[any:any]]:
+def read_from_json(path: Path) -> list[dict[Any, Any]]:
     with open(path, "r") as file:
         data = json.load(file)
 
