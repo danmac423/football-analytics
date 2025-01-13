4c72ad2001488b38da034a28d8b3d96d89760b61
dmachnia <01178558@pw.edu.pl>
2025-01-13 18:26:10 +0100

    Add service addresses for ball and player inference services

--- services/config.py
+++ services/config.py
@@ -1,12 +1,16 @@
 import torch
 
+INFERENCE_MANAGER_SERVICE_ADDRESS = "localhost:50051"
 
 BALL_INFERENCE_MODEL_PATH = "models/ball_inference.pt"
+BALL_INFERENCE_SERVICE_ADDRESS = "localhost:50052"
 
-PLAYER_INFERENCE_MODEL_PATH="models/player_inference.pt"
+PLAYER_INFERENCE_MODEL_PATH = "models/player_inference.pt"
+PLAYER_INFERENCE_SERVICE_ADDRESS = "localhost:50053"
 
-DEVICE="cpu"
+
+DEVICE = "cpu"
 if torch.cuda.is_available():
-    DEVICE="cuda"
+    DEVICE = "cuda"
 elif torch.mps.is_available():
     DEVICE = "mps"
