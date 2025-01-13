26bea7074e2b37be7ac71e61c6b43d2cb6115ba5
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-13 17:46:15 +0100

    FA-51 Add remove_ball_label_from_data_yaml and remove_label_zero

--- ai/config_io.py
+++ ai/config_io.py
@@ -1,7 +1,11 @@
 import json
+import os
 
 from pathlib import Path
 from typing import Any
+from loguru import logger
+
+import yaml
 
 
 def write_to_json(path: Path, data: list[dict[Any, Any]]) -> None:
@@ -17,3 +21,45 @@ def read_from_json(path: Path) -> list[dict[Any, Any]]:
             return [data]
         else:
             return data
+
+
+def remove_ball_label_from_data_yaml(file_path: str):
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+
+    data['nc'] = 3
+
+    if 'names' in data and 'ball' in data['names']:
+        data['names'].remove('ball')
+
+    with open(file_path, 'w') as file:
+        yaml.dump(data, file, default_flow_style=False)
+
+    logger.info(f"YAML file at {file_path} modified successfully!")
+
+
+def remove_label_zero(dataset_directory: str):
+    if os.path.isfile(dataset_directory):
+        dataset_directory = os.path.dirname(dataset_directory)
+
+    dataset_to_clear = ["test", "train", "valid"]
+
+    for dataset_to_clear in dataset_to_clear:
+        for file_name in os.listdir(f"{dataset_directory}/{dataset_to_clear}/labels"):
+            file_path = f"{dataset_directory}/{dataset_to_clear}/labels/{file_name}"
+
+            with open(file_path, "r+") as f:
+                lines = f.readlines()
+
+                f.seek(0)
+                f.truncate(0)
+
+                for line in lines:
+                    if line.startswith("0"):
+                        continue
+
+                    processed_line = " ".join(
+                        str(int(float(value)) - 1) if i == 0 else value
+                        for i, value in enumerate(line.split())
+                    )
+                    f.write(processed_line + "\n")
