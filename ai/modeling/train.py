7e60e78c4ebed8ad5e75972bbcf1b1755c583adf
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2025-01-13 17:45:45 +0100

    FA-51 Move remove_label function to config_io.py

--- ai/modeling/train.py
+++ ai/modeling/train.py
@@ -7,8 +7,7 @@ from loguru import logger
 from ultralytics import YOLO
 
 from ai.config import RUNS_DIR
-from ai.config_io import read_from_json
-
+from ai.config_io import read_from_json, remove_label_zero, remove_ball_label_from_data_yaml
 
 app = typer.Typer()
 
@@ -23,56 +22,6 @@ def train(config: dict):
     model.train(**config)
 
 
-def remove_label():
-    datasets_dirs = os.listdir(f"{HOME}/datasets")
-    datasets_dirs = [os.path.abspath(dir) for dir in datasets_dirs]
-
-    datasests_to_clear = ["test", "train", "valid"]
-
-    for datasets_dir in datasets_dirs:
-        for dataset_to_clear in datasests_to_clear:
-            for file_name in os.listdir(f"{datasets_dir}/{dataset_to_clear}/labels"):
-                file_path = f"{datasets_dir}/{dataset_to_clear}/labels/{file_name}"
-
-                with open(file_path, "r+") as f:
-                    lines = f.readlines()
-                    f.seek(0)
-                    f.truncate(0)
-                    for line in lines:
-                        if line.startswith("0"):
-                            continue
-
-                        processed_line = " ".join(
-                            str(int(float(value)) - 1) if i == 0 else value
-                            for i, value in enumerate(line.split())
-                        )
-                        f.write(processed_line + "\n")
-
-    datasets_dirs = os.listdir(f"{HOME}/datasets")
-    datasets_dirs = [os.path.abspath(dir) for dir in datasets_dirs]
-
-    datasests_to_clear = ["test", "train", "valid"]
-
-    for datasets_dir in datasets_dirs:
-        for dataset_to_clear in datasests_to_clear:
-            for file_name in os.listdir(f"{datasets_dir}/{dataset_to_clear}/labels"):
-                file_path = f"{datasets_dir}/{dataset_to_clear}/labels/{file_name}"
-
-                with open(file_path, "r+") as f:
-                    lines = f.readlines()
-                    f.seek(0)
-                    f.truncate(0)
-                    for line in lines:
-                        if line.startswith("0"):
-                            continue
-
-                        processed_line = " ".join(
-                            str(int(float(value)) - 1) if i == 0 else value
-                            for i, value in enumerate(line.split())
-                        )
-                        f.write(processed_line + "\n")
-
-
 @app.command()
 def main(training_config_path: Path):
     logger.info(f"Reading configuration from {training_config_path}")
@@ -81,10 +30,17 @@ def main(training_config_path: Path):
         current_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         logger.info(f"Current timestamp: {current_timestamp}")
 
+        config["data"] = os.path.abspath(config["data"])
+
         if "project" not in config.keys():
             config["project"] = RUNS_DIR / f"{config["model"][:-3]}" / f"train_{current_timestamp}"
 
         logger.info(f"Saving training run to: {config['project']}")
+
+        if "remove_label" in config.keys():
+            remove_ball_label_from_data_yaml(config["data"])
+            remove_label_zero(config["data"])
+            config.pop("remove_label")
 
         train(config)
 
