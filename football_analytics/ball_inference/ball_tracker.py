f44566ad45a82edd94677ac2ae7e993d9ea49f2a
Natalia <npieczko23@gmail.com>
2025-01-15 19:57:33 +0100

    Refactor YOLOBallInferenceService to use YOLOBallInterer for ball detection and tracking

---
+++ football_analytics/ball_inference/ball_tracker.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+import numpy as np
+import supervision as sv
+
+
+class BallTracker:
+    def __init__(self, buffer_size: int = 10):
+        self.buffer = deque(maxlen=buffer_size)
+
+    def update(self, detections: sv.Detections) -> sv.Detections:
+        xy = detections.get_anchors_coordinates(sv.Position.CENTER)
+        self.buffer.append(xy)
+
+        if len(detections) == 0:
+            return detections
+
+        centroid = np.mean(np.concatenate(self.buffer), axis=0)
+        distances = np.linalg.norm(xy - centroid, axis=1)
+        index = np.argmin(distances)
+        return detections[[index]]
+
+    def reset(self):
+        self.buffer.clear()
