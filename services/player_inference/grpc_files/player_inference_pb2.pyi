e61a17ebf787f978cc9f431f6d41586d5b4a191f
dmachnia <01178558@pw.edu.pl>
2025-01-15 12:31:18 +0100

    Add tracker_id to BoundingBox and update player inference logic

--- services/player_inference/grpc_files/player_inference_pb2.pyi
+++ services/player_inference/grpc_files/player_inference_pb2.pyi
@@ -17,9 +17,7 @@ class Frame(_message.Message):
     frame_id: int
     content: bytes
     def __init__(
-        self,
-        frame_id: _Optional[int] = ...,
-        content: _Optional[bytes] = ...
+        self, frame_id: _Optional[int] = ..., content: _Optional[bytes] = ...
     ) -> None: ...
 
 class PlayerInferenceResponse(_message.Message):
@@ -31,23 +29,25 @@ class PlayerInferenceResponse(_message.Message):
     def __init__(
         self,
         frame_id: _Optional[int] = ...,
-        boxes: _Optional[_Iterable[_Union[BoundingBox, _Mapping]]] = ...
+        boxes: _Optional[_Iterable[_Union[BoundingBox, _Mapping]]] = ...,
     ) -> None: ...
 
 class BoundingBox(_message.Message):
-    __slots__ = ("x1_n", "y1_n", "x2_n", "y2_n", "confidence", "class_label")
+    __slots__ = ("x1_n", "y1_n", "x2_n", "y2_n", "confidence", "class_label", "tracker_id")
     X1_N_FIELD_NUMBER: _ClassVar[int]
     Y1_N_FIELD_NUMBER: _ClassVar[int]
     X2_N_FIELD_NUMBER: _ClassVar[int]
     Y2_N_FIELD_NUMBER: _ClassVar[int]
     CONFIDENCE_FIELD_NUMBER: _ClassVar[int]
     CLASS_LABEL_FIELD_NUMBER: _ClassVar[int]
+    TRACKER_ID_FIELD_NUMBER: _ClassVar[int]
     x1_n: float
     y1_n: float
     x2_n: float
     y2_n: float
     confidence: float
     class_label: str
+    tracker_id: int
     def __init__(
         self,
         x1_n: _Optional[float] = ...,
@@ -55,5 +55,6 @@ class BoundingBox(_message.Message):
         x2_n: _Optional[float] = ...,
         y2_n: _Optional[float] = ...,
         confidence: _Optional[float] = ...,
-        class_label: _Optional[str] = ...
+        class_label: _Optional[str] = ...,
+        tracker_id: _Optional[int] = ...,
     ) -> None: ...
--- services/player_inference/player_inference_service.py
+++ services/player_inference/player_inference_service.py
@@ -7,6 +7,7 @@ from typing import Any, Generator, Iterator
 import cv2
 import grpc
 import numpy as np
+import supervision as sv
 from ultralytics import YOLO
 from ultralytics.engine.results import Results
 
@@ -51,30 +52,63 @@ class YOLOPlayerInferenceServiceServicer(
             Generator[player_inference_pb2.PlayerInferenceResponse, Any, Any]: returns the response
             with frame_id and boxes
         """
+        tracker = sv.ByteTrack(
+            lost_track_buffer=100,
+            minimum_consecutive_frames=3,
+        )
+
         for frame in request_iterator:
             try:
                 frame_image = cv2.imdecode(
                     np.frombuffer(frame.content, np.uint8), cv2.IMREAD_COLOR
                 )
+                height, width, _ = frame_image.shape
 
                 results: Results = self.model(frame_image)[0]
+                detections = sv.Detections.from_ultralytics(results)
+                detections = tracker.update_with_detections(detections)
+
                 labels = results.names
 
                 boxes = []
 
-                for box in results.boxes:
-                    coordinates = box.xyxyn.cpu().numpy().flatten()
-                    x1_n, y1_n, x2_n, y2_n = coordinates[:4]
+                for box, conf, cls, tracker_id in zip(
+                    detections.xyxy,
+                    detections.confidence,
+                    detections.class_id,
+                    detections.tracker_id,
+                ):
+                    x1, y1, x2, y2 = box[:4]
+                    x1_n = x1 / width
+                    y1_n = y1 / height
+                    x2_n = x2 / width
+                    y2_n = y2 / height
+
                     boxes.append(
                         player_inference_pb2.BoundingBox(
                             x1_n=x1_n,
                             y1_n=y1_n,
                             x2_n=x2_n,
                             y2_n=y2_n,
-                            confidence=box.conf.item(),
-                            class_label=labels[int(box.cls.item())],
+                            confidence=conf,
+                            class_label=labels[cls],
+                            tracker_id=tracker_id,
                         )
                     )
+
+                # for box in results.boxes:
+                #     coordinates = box.xyxyn.cpu().numpy().flatten()
+                #     x1_n, y1_n, x2_n, y2_n = coordinates[:4]
+                #     boxes.append(
+                #         player_inference_pb2.BoundingBox(
+                #             x1_n=x1_n,
+                #             y1_n=y1_n,
+                #             x2_n=x2_n,
+                #             y2_n=y2_n,
+                #             confidence=box.conf.item(),
+                #             class_label=labels[int(box.cls.item())],
+                #         )
+                # )
                 logger.info(f"Frame ID {frame.frame_id} processed with {len(boxes)} detections.")
 
                 yield player_inference_pb2.PlayerInferenceResponse(
